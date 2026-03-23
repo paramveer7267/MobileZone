b6ea01c6996c4043adb56400802ce69255bee5f3
Paramveer Singh <paramveer7267@gmail.com>
2026-03-23 16:52:46 +0530

    v1.0.0 released

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -148,7 +148,7 @@ const Footer = () => {
         {/* Bottom Bar */}
         <div className="border-t border-gray-800 mt-12 pt-8 flex flex-col md:flex-row justify-between items-center">
           <p className="text-gray-400">
-            © {new Date().getFullYear()} MobileZone. All rights reserved.
+            © {new Date().getFullYear()} MobileZone v1.0.0. All rights reserved.
           </p>
           <div className="flex space-x-6 mt-4 md:mt-0">
             <Link to="#" className="text-gray-400 hover:text-primary">
